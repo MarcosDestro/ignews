709bdd03f1a5a86162cbeb286a326c51e8bb8520
MarcosDestro <marcosrdmoraes@gmail.com>
2022-06-14 18:23:23 -0300

    Finish Layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,32 @@
 import Head from '../../node_modules/next/head'
+import { SubcribeButton } from '../components/SubscribeButton/index';
+
+import styles from './home.module.scss';
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>ig.news | Início</title>
+        <title>Home | ig.news</title>
         
       </Head>
 
-      <h1>
-        Hello World 2
-      </h1>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, Welcome</span>
+          
+          <h1>News about the <span>React</span> world.</h1>
+          
+          <p>
+            Get access to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+
+          <SubcribeButton />
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
     </>
   )
 }
